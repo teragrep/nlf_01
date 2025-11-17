207ff0998a97fd042eb24f5c18614df2434aed00
Moonbow-1 <146731818+MoonBow-1@users.noreply.github.com>
2025-11-17 10:30:12 +0200

    Add LogicAppWorkflowRuntime support (#48)
    * Add LogicAppWorkflowRuntimeType and unit tests for it
    
    * Implement LogicAppWorkflowRuntimeType as an option to NLFPlugin.syslogMessage
    
    * Wrap WorkflowName in ASCIIString
    
    * Make LogicAppWorkflowRuntimeTypeTest a final class

--- src/main/java/com/teragrep/nlf_01/NLFPlugin.java
+++ src/main/java/com/teragrep/nlf_01/NLFPlugin.java
@@ -109,6 +109,9 @@ public final class NLFPlugin implements Plugin {
             else if (jsonObject.getString("Type").equals("FunctionAppLogs")) {
                 eventTypes.add(new FunctionAppLogsType(parsedEvent, realHostname));
             }
+            else if (jsonObject.getString("Type").equals("LogicAppWorkflowRuntime")) {
+                eventTypes.add(new LogicAppWorkflowRuntimeType(parsedEvent, realHostname));
+            }
             else if (jsonObject.getString("Type").endsWith("_CL")) {
                 eventTypes.add(new CLType(parsedEvent, realHostname));
             }
