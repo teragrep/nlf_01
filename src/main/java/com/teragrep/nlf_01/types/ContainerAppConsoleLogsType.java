fb4d0a2336c7e465aa81755294449d0488814ae7
MoonBow-1 <146731818+MoonBow-1@users.noreply.github.com>
2025-11-28 13:32:46 +0200

    Implement a new HashableRFC5424AppName to ensure that appNames are less than 48 characters long

--- src/main/java/com/teragrep/nlf_01/types/ContainerAppConsoleLogsType.java
+++ src/main/java/com/teragrep/nlf_01/types/ContainerAppConsoleLogsType.java
@@ -49,9 +49,9 @@ import com.teragrep.akv_01.event.ParsedEvent;
 import com.teragrep.akv_01.plugin.PluginException;
 import com.teragrep.nlf_01.PropertiesJson;
 import com.teragrep.nlf_01.util.ASCIIString;
+import com.teragrep.nlf_01.util.HashableRFC5424AppName;
 import com.teragrep.nlf_01.util.MD5Hash;
 import com.teragrep.nlf_01.util.ResourceId;
-import com.teragrep.nlf_01.util.ValidRFC5424AppName;
 import com.teragrep.nlf_01.util.ValidRFC5424Hostname;
 import com.teragrep.nlf_01.util.ValidRFC5424Timestamp;
 import com.teragrep.rlo_14.Facility;
@@ -113,7 +113,7 @@ public final class ContainerAppConsoleLogsType implements EventType {
 
         assertKey(record, "ContainerAppName", JsonValue.ValueType.STRING);
 
-        return new ValidRFC5424AppName(record.getString("ContainerAppName")).appName();
+        return new HashableRFC5424AppName(record.getString("ContainerAppName")).appName();
     }
 
     @Override
---
+++ src/main/java/com/teragrep/nlf_01/util/HashableRFC5424AppName.java
@@ -0,0 +1,76 @@
+/*
+ * Teragrep Neon log format plugin for AKV_01
+ * Copyright (C) 2025 Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
+package com.teragrep.nlf_01.util;
+
+import com.teragrep.akv_01.plugin.PluginException;
+
+public final class HashableRFC5424AppName implements RFC5424AppName {
+
+    private final static int MAX_LENGTH = 48;
+    private final String rfc5424AppName;
+
+    public HashableRFC5424AppName(final String rfc5424AppName) {
+        this.rfc5424AppName = rfc5424AppName;
+    }
+
+    /**
+     * Returns a {@link ValidRFC5424AppName} without further modification if {@link #rfc5424AppName} if shorter than 48
+     * characters long. Otherwise, creates an MD5 hash of that field and shortens that to less than 48 characters.
+     */
+    @Override
+    public String appName() throws PluginException {
+        final String returnedRFC5424AppName;
+
+        if (this.rfc5424AppName.length() > MAX_LENGTH) {
+            final String md5HashedAppName = "md5-".concat(new MD5Hash(rfc5424AppName).md5()); // Will be 36 characters long
+            returnedRFC5424AppName = new ValidRFC5424AppName(md5HashedAppName).appName();
+        }
+        else {
+            returnedRFC5424AppName = new ValidRFC5424AppName(this.rfc5424AppName).appName();
+        }
+        return returnedRFC5424AppName;
+    }
+}
