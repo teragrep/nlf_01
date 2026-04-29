70c36a43bf87b77fad0920f7c3a129a751f8ca7b
Moonbow-1 <146731818+MoonBow-1@users.noreply.github.com>
2026-04-29 10:49:06 +0300

    Add support for "Event" eventType (#83)
    * Implement WindowsEventType and its tests
    
    * Add WindowsEventType to NLFPlugin.syslogMessage

--- src/test/java/com/teragrep/nlf_01/NLFPluginTest.java
+++ src/test/java/com/teragrep/nlf_01/NLFPluginTest.java
@@ -79,6 +79,7 @@ import com.teragrep.nlf_01.types.PowerAutomateActivityType;
 import com.teragrep.nlf_01.types.PowerPlatformAdminActivityType;
 import com.teragrep.nlf_01.types.SQLSecurityAuditEventsType;
 import com.teragrep.nlf_01.types.SyslogType;
+import com.teragrep.nlf_01.types.WindowsEventType;
 import com.teragrep.nlf_01.util.Sourceable;
 import com.teragrep.rlo_14.SDElement;
 import com.teragrep.rlo_14.SDParam;
@@ -501,6 +502,39 @@ public class NLFPluginTest {
         Assertions
                 .assertEquals(DataverseActivityType.class.getSimpleName(), sdElementMap.get("nlf_01@48577").get("eventType"));
 
+        Assertions.assertTrue(sdElementMap.get("aer_event@48577").containsKey("properties"));
+    }
+
+    @Test
+    void windowsEventTypeTest() {
+        final String json = Assertions
+                .assertDoesNotThrow(() -> Files.readString(Paths.get("src/test/resources/windows_event.json")));
+        final ParsedEvent parsedEvent = new ParsedEventFactory(
+                new UnparsedEventImpl(json, new EventPartitionContextImpl(new HashMap<>()), new EventPropertiesImpl(new HashMap<>()), new EventSystemPropertiesImpl(new HashMap<>()), new EnqueuedTimeImpl("2020-01-01T00:00:00"), new EventOffsetImpl("0"))
+        ).parsedEvent();
+
+        final NLFPlugin plugin = new NLFPlugin(new FakeSourceable());
+        final List<SyslogMessage> syslogMessages = Assertions
+                .assertDoesNotThrow(() -> plugin.syslogMessage(parsedEvent));
+        Assertions.assertEquals(1, syslogMessages.size());
+
+        final SyslogMessage syslogMessage = syslogMessages.get(0);
+        Assertions.assertEquals("md5-0ded52ef915af563e25778bf26b0f129-resourceName", syslogMessage.getHostname());
+        Assertions.assertEquals("Windows", syslogMessage.getAppName());
+        Assertions.assertEquals("2020-01-01T01:02:34.567Z", syslogMessage.getTimestamp());
+        Assertions.assertEquals(json, syslogMessage.getMsg());
+        final Map<String, Map<String, String>> sdElementMap = syslogMessage
+                .getSDElements()
+                .stream()
+                .collect(Collectors.toMap((SDElement::getSdID), (sdElem) -> sdElem.getSdParams().stream().collect(Collectors.toMap(SDParam::getParamName, SDParam::getParamValue))));
+
+        Assertions
+                .assertEquals(
+                        "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}",
+                        sdElementMap.get("origin@48577").get("_ResourceId")
+                );
+        Assertions
+                .assertEquals(WindowsEventType.class.getSimpleName(), sdElementMap.get("nlf_01@48577").get("eventType"));
         Assertions.assertTrue(sdElementMap.get("aer_event@48577").containsKey("properties"));
     }
 
